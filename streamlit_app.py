8b2bcb3483ec858eb989f1a2cad59bfd65740358
Aliaksandr Kudzelka <113917233+akudzelka@users.noreply.github.com>
2022-09-19 17:45:05 +0300

    Update streamlit_app.py
    Added one more header

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('Breakfast Menu')
 streamlit.text('🐔 Омлет')
 streamlit.text('🥑🍞 Тост с авокадо')
 streamlit.text('Чай чёрный, шоколад')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
