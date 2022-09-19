3b1203252977499ed92e03edef96b20c02b5a55a
Aliaksandr Kudzelka <113917233+akudzelka@users.noreply.github.com>
2022-09-19 17:39:16 +0300

    Update streamlit_app.py
    Added Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Омлет')
+streamlit.text('Тост с авокадо')
+streamlit.text('Чай чёрный, шоколад')
