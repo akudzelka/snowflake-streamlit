bc53181a2180960ab57ec9ce6672005a3bcd9b36
Aliaksandr Kudzelka <113917233+akudzelka@users.noreply.github.com>
2022-09-19 17:44:23 +0300

    Update streamlit_app.py
    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Омлет')
-streamlit.text('Тост с авокадо')
+streamlit.text('🐔 Омлет')
+streamlit.text('🥑🍞 Тост с авокадо')
 streamlit.text('Чай чёрный, шоколад')
